505bd8a7dff861342bc01de7dc0860758e180573
issacchern <issac.chua12@gmail.com>
2016-02-26 02:18:43 -0500

    finish cliptoBoard and share intent

--- app/src/main/java/com/chernyee/cssquare/QuestionActivity.java
+++ app/src/main/java/com/chernyee/cssquare/QuestionActivity.java
@@ -1,6 +1,9 @@
 package com.chernyee.cssquare;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
@@ -30,6 +33,7 @@ public class QuestionActivity extends AppCompatActivity {
     private Button codeButton;
     private int hintCount = 0;
     private CheckBox codeCheck;
+    private String cliptoBoard;
 
     private SharedPreferences sharedPref;
 
@@ -104,6 +108,7 @@ public class QuestionActivity extends AppCompatActivity {
 
         updateCodeAndNumber(info.get(3));
 
+        cliptoBoard = info.get(3);
 
         codeButton = (Button) findViewById(R.id.codeButton);
         codeButton.setOnClickListener(new View.OnClickListener() {
@@ -112,11 +117,13 @@ public class QuestionActivity extends AppCompatActivity {
 
                 if(codeButton.getText().equals("Hide Solution")){
                     updateCodeAndNumber(info.get(3));
+                    cliptoBoard = info.get(3);
                     codeButton.setText("Show Solution");
                     codeCheck.setVisibility(View.VISIBLE);
 
                 } else{
                     updateCodeAndNumber(info.get(4));
+                    cliptoBoard = info.get(4);
                     codeButton.setText("Hide Solution");
                     codeCheck.setVisibility(View.VISIBLE);
 
@@ -159,16 +166,24 @@ public class QuestionActivity extends AppCompatActivity {
 
 
 
-
-//        final FloatingActionsMenu menuMultipleActions = (FloatingActionsMenu) findViewById(R.id.multiple_actions);
-//        menuMultipleActions.addButton(actionC);
-
-
         final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
         actionA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                actionA.setTitle("Code copied!");
+
+                if(actionA.getTitle().equals("Code copied!")){
+                    actionA.setTitle("Code uncopied!");
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("label", "nanana");
+                    clipboard.setPrimaryClip(clip);
+                }else{
+                    actionA.setTitle("Code copied!");
+                    ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("label", cliptoBoard);
+                    clipboard.setPrimaryClip(clip);
+                }
+
+
             }
         });
 
@@ -176,6 +191,12 @@ public class QuestionActivity extends AppCompatActivity {
         actionB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, cliptoBoard);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
 
             }
         });
